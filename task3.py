867b77e79163d7c30d66ec24b3fabd075729529c
kamila-velazquez533 <kamila.velazquez533@students.cmcss.net>
2026-03-05 08:06:28 -0600

    KV

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 #Task 3 - The Guard
 guard_awake = False
 
-if guard_awake:
+if not guard_awake:
     outcome = "Shadow: You slip past the guard unnoticed."
 else:
     outcome = "Doom: The guard spots you and saises the alarm."
