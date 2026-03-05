13ba9ddca207ac9edf1761eb971bb9ac3b7852bd
kamila-velazquez533 <kamila.velazquez533@students.cmcss.net>
2026-03-05 07:52:53 -0600

    KV

---
+++ task1.py
@@ -0,0 +1,8 @@
+#Task 1 - The Torch
+torch_lit = True
+
+if torch_lit:
+    outcome = "Flicker: The flame glows warmly, revelating a hidden passage."
+else:
+    outcome = "Doom: Darkness swallows the room as shadows close in."
+print(outcome)
---
+++ task3.py
@@ -0,0 +1,8 @@
+#Task 3 - The Guard
+guard_alert = True
+
+if guard_alert:
+    outcome = "Shadow: You slip past the guard unnoticed."
+else:
+    outcome = "Doom: The guard spots you and saises the alarm."
+print(outcome)
